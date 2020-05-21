5124ee08752e1d95c9fd709560ed50e4ba66e761
Matthias Urlichs <matthias@urlichs.de>
2020-05-21 20:11:48 +0200

    fix gen.client.id

--- distmqtt/utils.py
+++ distmqtt/utils.py
@@ -36,10 +36,10 @@ def gen_client_id():
     :return:
     """
     import random
-    gen_id = 'distmqtt/'
+    gen_id = 'distmqtt-'
 
-    for i in range(7, 23):
-        gen_id += chr(random.randint(0, 74) + 48)
+    for i in range(16):
+        gen_id += chr(random.randint(0, 26) + 97)
     return gen_id
 
 
