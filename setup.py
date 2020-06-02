bc5f6b8bf93e3ee9c5ec090a6e298ee7706ac587
Matthias Urlichs <matthias@urlichs.de>
2020-06-02 09:33:31 +0200

    Default to testing with pytest

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     name="distmqtt",
     use_scm_version={"version_scheme": "guess-next-dev", "local_scheme": "dirty-tag"},
     setup_requires=["setuptools_scm", "pytest-runner"],
+    tests_require=['pytest'],
     description="MQTT client/broker using anyio and distkv",
     author="Matthias Urlichs",
     author_email='matthias@urlichs.de',
